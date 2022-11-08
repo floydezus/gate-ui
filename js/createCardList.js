bccf0dc3b7bcd4e36d197a7c50ded9ee60bc0ba0
aleksandr.tokarev <oulfc93@gmail.com>
2022-11-08 22:21:08 +0100

    Updates

---
+++ js/createCardList.js
@@ -0,0 +1,13 @@
+export function createCardListWithTemplate(dataCards) {
+    const ul = document.createElement('ul');
+    const placeholder = document.getElementById('list-placeholder')
+    ul.classList.add('list', 'card-list');
+    const template = document.getElementById('card-template');
+    dataCards.forEach((card) => {
+        const cardNode = document.importNode(template.content, true);
+        cardNode.querySelector('.title').textContent = card?.title;
+        cardNode.querySelector('.description').textContent = card?.body;
+        ul.appendChild(cardNode);
+    });
+    placeholder.replaceWith(ul);
+}
